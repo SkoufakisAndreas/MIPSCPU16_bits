3ea500e4e9e3941794cf70e1f9a977cd5328f62e
ANDREAS SKOUFAKIS <skoufakisandre@gmail.com>
2026-04-03 15:37:58 +0300

    Change register size and update read/write logic

--- src/regfile.v
+++ src/regfile.v
@@ -1,23 +1,23 @@
 module regfile (
     input wire clk,                  
-    input wire [3:0] readReg1, 
-    input wire [3:0] readReg2, 
-    input wire [3:0] writeReg,
+    input wire [2:0] readReg1, 
+    input wire [2:0] readReg2, 
+    input wire [2:0] writeReg,
     input wire [15:0] writeData,
     input wire RegWrite,
     output wire [15:0] readData1,    
     output wire [15:0] readData2
 );
 
-    reg [15:0] registers [15:0];
+    reg [15:0] registers [7:0];
 
     
-    assign readData1 = (readReg1 == 4'b0000) ? 16'h0000 : registers[readReg1];
-    assign readData2 = (readReg2 == 4'b0000) ? 16'h0000 : registers[readReg2];
+    assign readData1 = (readReg1 == 3'b000) ? 16'h0000 : registers[readReg1];
+    assign readData2 = (readReg2 == 3'b000) ? 16'h0000 : registers[readReg2];
 
     
     always @(posedge clk) begin
-        if (RegWrite && writeReg != 4'b0000) begin
+        if (RegWrite && writeReg != 3'b000) begin
             registers[writeReg] <= writeData;
         end
     end
