6e36ce90990ab050ab4e83044b5b26ae97d415da
ANDREAS SKOUFAKIS <skoufakisandre@gmail.com>
2026-04-09 01:23:05 +0300

    Refactor PC module to use 3-bit address and enable input

--- src/pc.v
+++ src/pc.v
@@ -1,27 +1,15 @@
 module PC(
-  input clk,
-  input rst,
-  input jump,
-  input [15:0] jump_address,
-  output reg [15:0] pc_out
+    input wire clk, rst, en, jump,
+    input wire [2:0] jump_address,
+    output reg [2:0] pc_out
 );
-
-  reg [15:0] p_c;
-  wire [15:0] pc_next;
-
-  assign pc_next = jump ? jump_address : (p_c + 1);
-
-  always @(posedge clk) begin
-    if (!rst)
-      p_c <= 16'd0;
-    else if (p_c >= 16'd15)  // Only 16 instructions
-      p_c <= 16'd0;
-    else
-      p_c <= pc_next;
-  end
-
-  always @(*) begin
-    pc_out = p_c;
-  end
-
+    
+    always @(posedge clk) begin
+        if (rst) begin
+            pc_out <= 3'd0; // Active-High reset (όπως το έχουμε στο top module)
+        end else if (en) begin
+           
+            pc_out <= jump ? jump_address : (pc_out + 1);
+        end
+    end
 endmodule
